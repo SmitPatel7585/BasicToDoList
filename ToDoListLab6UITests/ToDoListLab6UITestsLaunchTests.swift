3439522094afa38fc655fb232312262d977670d4
Smit Patel <smit.patel7585@gmail.com>
2024-02-18 12:33:17 -0500

    Initial Commit

---
+++ ToDoListLab6UITests/ToDoListLab6UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ToDoListLab6UITestsLaunchTests.swift
+//  ToDoListLab6UITests
+//
+//  Created by user235217 on 2/18/24.
+//
+
+import XCTest
+
+final class ToDoListLab6UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
